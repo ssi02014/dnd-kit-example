0300e5307474af648cb5b9921d077579fd97153f
ssi02014 <ssi02014@naver.com>
2022-12-07 00:20:59 +0900

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import GlobalStyle from './style/GlobalStyle';
+import SortableList from './components/SortableList';
+import SortableItem from './components/SortableItem';
 
+interface Items {
+  id: number | string;
+  value: string;
+}
 function App() {
+  const [items, setItems] = useState<Items[]>([
+    { id: 'aa', value: 'aa' },
+    { id: 'bb', value: 'bb' },
+    { id: 'cc', value: 'cc' },
+    { id: 'dd', value: 'dd' },
+    { id: 'ee', value: 'ee' },
+    { id: 'ff', value: 'ff' },
+    { id: 'gg', value: 'gg' },
+    { id: 'hh', value: 'hh' },
+    { id: 'ii', value: 'ii' },
+    { id: 'jj', value: 'jj' },
+  ]);
+
+  const onChange = (items: any) => {
+    setItems(items);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <Wrapper>
+        <SortableList
+          items={items}
+          onChange={onChange}
+          renderItem={(item: Items) => (
+            <SortableItem id={item.id}>{item.value}</SortableItem>
+          )}
+        />
+      </Wrapper>
+    </>
   );
 }
+
+const Wrapper = styled.div`
+  width: 800px;
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+`;
 
 export default App;
